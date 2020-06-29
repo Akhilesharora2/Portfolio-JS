ba430390c1864fc868e652f2187f4fdb516b5408
Akhilesh Arora <200428489@student.georgianc.on.ca>
2020-06-29 02:11:05 -0400

    Header, Footer and Navbar

--- Portfolio/routes/index.js
+++ Portfolio/routes/index.js
@@ -1,10 +1,19 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
+
+app.get('/contact', function (req, res) {
+    console.log("woring");
+   // res.send('contact');
+});
+
+//app.get('/project', function (req, res) {
+//    res.render('project', { name: 'akhil' });
+//});
 
 module.exports = router;
